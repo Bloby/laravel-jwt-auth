d7dfa77b1bd98fd832e3d6a447cdaf0aef88b4f8
Yevhenii <zvezdnev@gmail.com>
2017-08-16 01:12:29 +0300

    fix

--- src/Listeners/LoginListener.php
+++ src/Listeners/LoginListener.php
@@ -28,10 +28,10 @@ class LoginListener
     public function handle(Login $event)
     {
         $attemptKey = sprintf('attempt.%s', $event->clientID);
-        if ($event->cache->has($attemptKey) && $event->cache->get($attemptKey) < config('jwt.attempts')) {
-            $event->cache->forget($attemptKey);
+        if ($event->cache->has($attemptKey) && (int)$event->cache->get($attemptKey) >= (int)config('jwt.attempts')) {
+            throw new AttemptException(sprintf('You are blocked on %s min! You made many attempts at authentication.', config('jwt.attempts')));
         }
-
-        throw new AttemptException(sprintf('You are blocked on %s min! You made many attempts at authentication.', config('jwt.attempts')));
+        
+        $event->cache->forget($attemptKey);
     }
 }
